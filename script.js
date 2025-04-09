3040d881dbfb80dc27407415ab5999b61520f8b0
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-04-09 16:05:28 +0200

    feat(script):Ejercicio 6

--- script.js
+++ script.js
@@ -59,8 +59,13 @@ switch (pet) {
 //Ejercicio 6: Usando un operador ternario, escribe un programa que lea la constante weather e imprima en consola si es soleado "Me vestiré con un vestido" y si no es así que imprima "Me vestiré con pantalón"
 
 const weather = "soleado";
+//Escribe tu código aquí;
+const result =
+  weather == "soleado"
+    ? "Me vestiré con un vestido"
+    : "Me vestiré con pantalón";
 
-const result = console.log(result); //Escribe tu código aquí;
+console.log(result);
 
 //Ejercicio 7: cambia el valor de weather a "frío" y mira el resultado en consola para comprobar que tu código funciona bien.
 
